3cbf5b01a7459f1f64f9953d5855c5dae8f5ee8b
Daniel Nogales Iglesias <44336888+proyectoarchlinux@users.noreply.github.com>
2019-10-23 13:37:36 +0200

    Update install32.sh

--- openjdk-8-jre-install/install32.sh
+++ openjdk-8-jre-install/install32.sh
@@ -4,9 +4,9 @@ WGET="wget http://ftp.debian.org/debian/pool/main/o/openjdk-8/openjdk-8-jre-head
 
 WGETJAVA="wget http://ftp.debian.org/debian/pool/main/o/openjdk-8/openjdk-8-jre_8u222-b10-1~deb9u1_i386.deb"
 
-INSTALL="sudo dpkg -i openjdk-8-jre-headless_8u212-b01-1~deb9u1_i386.deb"
+INSTALL="sudo dpkg -i openjdk-8-jre-headless_8u222-b10-1~deb9u1_i386.deb"
 
-INSTALLJAVA="sudo dpkg -i openjdk-8-jre_8u212-b01-1~deb9u1_i386.deb"
+INSTALLJAVA="sudo dpkg -i openjdk-8-jre_8u222-b10-1~deb9u1_i386.deb"
 
 echo instalacion...
 
